c303d73e99555597e9ab48c7b6e6f777d45c0bce
GangDuo <5295036+GangDuo@users.noreply.github.com>
2024-04-22 13:15:36 +0900

    読み込むファイルをコマンドライン引数で指定する。

--- libs/kintone/index.js
+++ libs/kintone/index.js
@@ -1,6 +1,20 @@
+const path = require('node:path');
+const fs = require('node:fs/promises');
 const { KintoneRestAPIClient } = require("@kintone/rest-api-client");
 
 (async () => {
+    const filePath = path.resolve(process.argv[2] ?? '.');
+	console.log(filePath)
+    try {
+        const stats = await fs.stat(filePath)
+        if (!stats.isFile()) {
+            throw new Error('ファイルを指定してください！')
+        }
+    } catch (error) {
+        console.log(error)
+        return
+    }
+
     const client = new KintoneRestAPIClient({
         baseUrl: process.env.BASE_URL,
         auth: { apiToken: process.env.KINTONE_API_TOKEN },
