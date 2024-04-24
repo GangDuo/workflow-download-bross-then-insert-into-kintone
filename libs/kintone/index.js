ad11ce468a2d63f3ab66570b25cb5cd7753fc736
GangDuo <5295036+GangDuo@users.noreply.github.com>
2024-04-24 16:18:05 +0900

    csvからkintone api clientのaddAllRecordsに渡すrecords型へ変換する。

--- libs/kintone/index.js
+++ libs/kintone/index.js
@@ -20,6 +20,17 @@ const iconv = require('iconv-lite');
     fs.createReadStream(filePath)
         .pipe(iconv.decodeStream('Shift_JIS'))
         .pipe(csv.parse())
+        .pipe(csv.transform((record) => {
+            const requiredColumns = record.filter((clms, i) => [0, 1, 2, 4, 5, 7].includes(i));
+            return requiredColumns.reduce((ax, clm, i) => {
+                const labels = ["請求年月", "請求先コード", "ａｕ電話番号", "内訳項目名", "金額", "記事欄"];
+                ax[`${labels[i]}`] = { value: clm };
+                return ax;
+            }, { "請求先名": { value: "直営" }});
+        }, (err, result) => {
+            console.dir(result);
+            console.log(`結果取得: ${result.length}}`);
+        }))
         .pipe(csv.stringify({
             quoted: true
         }))
