ba4c29d2df60762748f2044e9143fc559308249d
GangDuo <5295036+GangDuo@users.noreply.github.com>
2024-04-26 17:11:14 +0900

    dotenvモジュールを使用して環境変数を取得する。

--- windows/run.ps1
+++ windows/run.ps1
@@ -1,5 +1,12 @@
 <#
+準備
+下記モジュールをインストールする。
+https://github.com/stopthatastronaut/poshdotenv
+
 使用例
 pwsh -ExecutionPolicy Bypass .\run.ps1
 #>
-echo $ENV:Path
+Import-Module $env:userprofile\Documents\PowerShell\Modules\dotenv\0.1.0\dotenv.psm1
+Set-DotEnv -Path ./.env
+echo $env:APP_ENV
+Remove-DotEnv
