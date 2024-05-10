68c1d7c58c7bcfec48d2762ca7932409f5282d16
GangDuo <5295036+GangDuo@users.noreply.github.com>
2024-05-10 16:36:05 +0900

    一時ディレクトリにファイルをダウンロードする。

--- windows/run.ps1
+++ windows/run.ps1
@@ -9,4 +9,14 @@ pwsh -ExecutionPolicy Bypass .\run.ps1
 Import-Module $env:userprofile\Documents\PowerShell\Modules\dotenv\0.1.0\dotenv.psm1
 Set-DotEnv -Path ./.env
 echo $env:APP_ENV
+
+$tmp = $env:TEMP | Join-Path -ChildPath $([System.Guid]::NewGuid().Guid)
+New-Item -ItemType Directory -Path $tmp
+echo $tmp
+node .\..\libs\bross\index.js "$tmp"
+
+# ここに処理を記載する。
+
+$tmp | Remove-Item -Recurse
+
 Remove-DotEnv
